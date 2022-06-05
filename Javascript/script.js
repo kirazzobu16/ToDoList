81c749c9ed270b6a603e2958f7202bac9bc67a1b
Burak Kiraz <kirazzobu16@gmail.com>
2022-06-05 11:28:20 +0300

    todolist projesi yapılmıştır.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,14 +1,15 @@
 //<li class="list-group-item">Dapibus ac facilisis in <span class="close">x</span></li>
-
 let AddDOM=document.querySelector("#Add")
 let UserLiDOM=document.querySelector("#ToDoList")
 let myToast=document.querySelector("#mytoast")
 let errorDOM=document.querySelector("#error")
 let DeleteDOM=document.querySelector("#delete")
-AddDOM.addEventListener("click",AddItem)
+AddDOM.addEventListener("click",AddItem) 
+
 //fonksiyon başı
 function AddItem(event)
 {
+
     let textDOM=document.querySelector("#text") 
     let liDOM= document.createElement("li")
     let spanDOM=document.createElement("span")
@@ -21,32 +22,22 @@ function AddItem(event)
     spanDOM.classList.add("close")
     UserLiDOM.append(liDOM)
     liDOM.appendChild(spanDOM)
-    let option={
-        animation:true,
-        delay:1000
-    }
+    
     let toast=new bootstrap.Toast(myToast)
-    toast.show(option) 
+    toast.show() 
     }
     //if sonu
     else {
-        let option={
-            animation:true,
-            delay:1000
-        }
         let errorToast=new bootstrap.Toast(errorDOM)
-        errorToast.show(option)        
+        errorToast.show()        
     }     
     spanDOM.addEventListener("click",DeleteItem)
     function DeleteItem() {
         spanDOM.remove()
         liDOM.remove()
-        let option={
-            animation:true,
-            delay:1000
-        }
+        localStorage.removeItem("list")     
         let DeleteToast=new bootstrap.Toast(DeleteDOM)
-        DeleteToast.show(option) 
+        DeleteToast.show() 
     
     }
 }
